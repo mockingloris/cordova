7a054fdfd825ba8f3481c3011f6a5b78b3018124
mauro <maurop123@gmail.com>
2017-09-19 15:08:03 -0400

    add app props. add v-content

--- template/src/App.vue
+++ template/src/App.vue
@@ -6,6 +6,7 @@
       :clipped="clipped"
       v-model="drawer"
       enable-resize-watcher
+      app
     >
       <v-list>
         <v-list-tile
@@ -22,7 +23,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed>
+    <v-toolbar fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
       <v-btn
         icon
@@ -56,26 +57,29 @@
       </v-btn>
     </v-toolbar>
     <main>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/static/img/v.png" alt="Vuetify.js" class="mb-5">
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-        </v-slide-y-transition>
-      </v-container>
+      <v-content>
+        <v-container fluid>
+          <v-slide-y-transition mode="out-in">
+            <v-layout column align-center>
+              <img src="/static/img/v.png" alt="Vuetify.js" class="mb-5">
+              <blockquote>
+                &#8220;First, solve the problem. Then, write the code.&#8221;
+                <footer>
+                  <small>
+                    <em>&mdash;John Johnson</em>
+                  </small>
+                </footer>
+              </blockquote>
+            </v-layout>
+          </v-slide-y-transition>
+        </v-container>
+      </v-content>
     </main>
     <v-navigation-drawer
       temporary
       :right="right"
       v-model="rightDrawer"
+      app
     >
       <v-list>
         <v-list-tile @click="right = !right">
@@ -86,7 +90,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed">
+    <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
