433d994d15f285a487d9735863490a164a42eee0
mauro <maurop123@gmail.com>
2017-10-09 17:40:29 -0400

    update to 0.16.4

--- template/src/App.vue
+++ template/src/App.vue
@@ -23,7 +23,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app>
+    <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
       <v-btn
         icon
