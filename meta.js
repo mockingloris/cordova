bf1afd5dbde4863793b0cf8fcfd6394c6d4cf6ee
Cairin <cairinmichie@gmail.com>
2018-01-11 11:51:02 +0200

    Fix meta completeMessage

--- meta.js
+++ meta.js
@@ -98,5 +98,5 @@ module.exports = {
     "test/e2e/**/*": "e2e",
     "src/router/**/*": "router"
   },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  cordova platform add android\n npm run android\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  cordova platform add android\n  npm run android\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
